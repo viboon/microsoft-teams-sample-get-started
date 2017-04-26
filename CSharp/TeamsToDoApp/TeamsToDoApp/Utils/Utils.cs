f69425a52781c44c8edc3fedd7a27527b65bde13
Siddharth Uppal <suppal@microsoft.com>
2017-04-25 17:14:44 -0700

    working compose extensions

--- CSharp/TeamsToDoApp/TeamsToDoApp/Utils/Utils.cs
+++ CSharp/TeamsToDoApp/TeamsToDoApp/Utils/Utils.cs
@@ -43,4 +43,68 @@ namespace TeamsToDoApp.Utils
         public string ChannelId { get; set; }
         public string CanvasUrl { get; set; }
     }
+
+    // TODO: Use the classes from the SDK
+    public class ComposeExtensionParameter
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class QueryOptions
+    {
+        public int Skip { get; set; }
+        public int Count { get; set; }
+    }
+
+    public class ComposeExtensionQuery
+    {
+        public string CommandId { get; set; }
+        public List<ComposeExtensionParameter> Parameters { get; set; }
+        public QueryOptions QueryOptions { get; set; }
+
+        public const string InvokeActivityName = "inputExtension/query";
+    }
+
+
+    public class InvokeResponse
+    {
+        public ComposeExtensionResult InputExtension { get; set; }
+    }
+
+    public class ComposeExtensionResult
+    {
+        public string AttachmentLayout { get; set; }
+        public string Type { get; set; }
+        public List<Attachment> Attachments { get; set; }
+    }
+
+    public class InvokeResponseAttachment : Attachment
+    {
+        public Attachment Preview { get; set; }
+
+        public InvokeResponseAttachment()
+        {
+        }
+
+        public InvokeResponseAttachment(Attachment attachment)
+        {
+            this.Content = attachment.Content;
+            this.ContentType = attachment.ContentType;
+            this.ContentUrl = attachment.ContentUrl;
+            this.Name = attachment.Name;
+            this.ThumbnailUrl = attachment.ThumbnailUrl;
+
+            this.Preview = new Attachment()
+            {
+                Content = attachment.Content,
+                ContentType = attachment.ContentType,
+                ContentUrl = attachment.ContentUrl,
+                Name = attachment.Name,
+                ThumbnailUrl = attachment.ThumbnailUrl,
+            };
+        }
+
+    }
+
 }
